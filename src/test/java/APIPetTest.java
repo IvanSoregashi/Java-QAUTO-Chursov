b565feb62871fb506a1327f9fca422ecb0df16f8
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-02-09 18:06:00 +0500

    second test

--- src/test/java/APIPetTest.java
+++ src/test/java/APIPetTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class APIPetTest {
 
@@ -33,5 +35,34 @@ public class APIPetTest {
         Assertions.assertEquals(200, response.statusCode());
     }
 
+    @Test
+    void checkUserResponseBody() {
+        String baseUrl = "https://petstore.swagger.io/v2/";
+        String body = """
+           {
+             "id": 0,
+             "username": "string",
+             "firstName": "string",
+             "lastName": "string",
+             "email": "string",
+             "password": "string",
+             "phone": "string",
+             "userStatus": 0
+           }""";
+
+
+        given()
+                .baseUri(baseUrl)
+                .header("accept", "application/json")
+                .header("Content-Type", "application/json")
+                .body(body).
+                when()
+                .post("user")
+                .then()
+                .statusCode(200)
+                .body("code", equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", notNullValue(String.class));
+    }
 
 }
